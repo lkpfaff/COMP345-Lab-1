acaccc22dc73f7beaea1685ee23cab38cbebbac6
liam <lpfaff@ithaca.edu>
2019-01-27 20:37:42 -0500

    Updated email validators

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -6,7 +6,7 @@ public class BankAccount {
     private double balance;
 
     /**
-     * @throws IllegalArgumentException if email is invalid
+     *
      */
     public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
@@ -34,9 +34,38 @@ public class BankAccount {
 
     }
 
+    /**
+     * Takes an email address and returns true or false depending on
+     * whether it is a valid address
+     *
+     * A valid email address must have no whitespace,
+     * must have an '@' in the middle,
+     * must have characters before and after the @,
+     * must have a '.' at least 2 characters to the left of the end of the address
+     * must have characters between the '@' and the '.'
+     *
+     * @param email address to validate
+     * @return true if email is valid, false if email is invalid
+     *
+     */
 
     public static boolean isEmailValid(String email){
         if (email.indexOf('@') == -1){
+            return false;
+        }
+        if (email.indexOf(' ') > 0){
+            return false;
+        }
+        if ((email.lastIndexOf('.') - email.lastIndexOf('@') == 1)){
+            return false;
+        }
+        if (email.indexOf('@') == 0){
+            return false;
+        }
+        if ((email.lastIndexOf('.') - email.lastIndexOf('@') < 0)){
+            return false;
+        }
+        if (email.length()- email.lastIndexOf('.') < 3){
             return false;
         }
         else {
